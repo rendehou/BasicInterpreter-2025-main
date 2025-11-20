c261d6f15c428e8b02d663e10e471c0acf389a66
atri <rendehou@sjtu.com>
2025-11-20 09:23:42 +0800

    Statement

--- include/Statement.hpp
+++ include/Statement.hpp
@@ -23,3 +23,77 @@ class Statement {
 
 // TODO: Other statement types derived from Statement, e.g., GOTOStatement,
 // LetStatement, etc.
+//Let
+class LetStatement : public Statement{
+ public:
+  LetStatement(std::string source, std::string varName, Expression* expr);
+  ~LetStatement() override;
+
+  void execute(VarState& state, Program& program) const;
+
+ private:
+  std::string varName_;
+  Expression* expr_;
+};
+//Print
+class PrintStatement : public Statement{
+ public:
+  PrintStatement(std::string source, Expression* expr);
+  ~PrintStatement() override;
+
+  void execute(VarState& state, Program& program) const;
+
+ private:
+  std::string varName_;
+  Expression* expr_;
+};
+//Input
+class  InputStatement : public Statement{
+  public:
+  InputStatement(std::string source, std::string varName);
+  ~InputStatement() override;
+
+  void execute(VarState& state, Program& program) const;
+
+ private:
+  std::string varName_;
+};
+//Goto
+class GotoStatement : public Statement{
+  public:
+  GotoStatement(std::string source,int num);
+  ~GotoStatement() override;
+
+  void execute(VarState& state,Program& program) const;
+
+  private:
+  int num_;
+};
+//Rem
+class RemStatement : public Statement{
+  public:
+  RemStatement(std::string source);
+  ~RemStatement() override;
+
+  void execute(VarState& state,Program& program) const;
+};
+//End
+class EndStatement : public Statement{
+  public:
+  EndStatement(std::string source);
+  ~EndStatement() override;
+
+  void execute(VarState& state,Program& program) const;
+};
+//If
+class IfStatement : public Statement{
+  public:
+  IfStatement(std::string source,Expression* expr_left,Expression* expr_right,int num,char op);
+  ~IfStatement() override;
+  
+  void execute(VarState& state,Program& program) const;
+  private:
+  Expression *expr_left_,*expr_right_;
+  int num_;
+  char op_;
+};
